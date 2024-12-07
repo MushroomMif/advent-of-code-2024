2b2cce028e5f38aa5ddd39868004248c80a83938
MushroomMif <mushroommif@yandex.ru>
2024-12-07 20:42:41 +0300

    Fix Day 7

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,60 +1,64 @@
-import java.math.BigInteger
-
 fun main() {
     // SETUP
     val input = readInput()?.lines() ?: return
-    val equations = buildMap<Long, List<Long>> {
+    val equations = buildMap<Long, MutableList<List<Long>>> {
         for (line in input) {
-            put(
-                line.substringBefore(':')
-                    .toLong(),
-                line.substringAfter(": ")
+            getOrPut(
+                line.substringBefore(':').toLong()
+            ) { mutableListOf() }
+                .add(line.substringAfter(": ")
                     .split(' ')
                     .map(String::toLong)
-            )
+                )
         }
     }
 
     // PART 1
     var sum = 0L
-    for ((requiredResult, numbers) in equations) {
-        // Unfortunately, I wasn't able to write this function on my own, so I used the Internet
-        fun calcPossibleResults(nextIndex: Int = 0, current: Long = 0L): List<Long> {
-            if (nextIndex == numbers.size) return listOf(current)
+    for ((requiredResult, allNumbers) in equations) {
+        for (numbers in allNumbers) {
+            // Unfortunately, I wasn't able to write this function on my own, so I used the Internet
+            fun calcPossibleResults(nextIndex: Int = 0, current: Long = 0L): List<Long> {
+                if (nextIndex == numbers.size) return listOf(current)
 
-            val nextNumber = numbers[nextIndex]
-            return calcPossibleResults(
-                nextIndex + 1, current + nextNumber
-            ) + calcPossibleResults(
-                nextIndex + 1, current * nextNumber
-            )
+                val nextNumber = numbers[nextIndex]
+                return calcPossibleResults(
+                    nextIndex + 1, current + nextNumber
+                ) + calcPossibleResults(
+                    nextIndex + 1, current * nextNumber
+                )
+            }
+
+            if (requiredResult in calcPossibleResults()) sum += requiredResult
         }
-
-        if (requiredResult in calcPossibleResults()) sum += requiredResult
     }
 
     println(sum)
 
     // PART 2
-    var bigSum = BigInteger.valueOf(0)
-    for ((requiredResult, numbers) in equations) {
-        // Unfortunately, I wasn't able to write this function on my own, so I used the Internet
-        fun calcPossibleResults(nextIndex: Int = 0, current: Long = 0L): List<Long> {
-            if (nextIndex == numbers.size) return listOf(current)
+    var bigSum = 0L
+    for ((requiredResult, allNumbers) in equations) {
+        for (numbers in allNumbers) {
+            // Unfortunately, I wasn't able to write this function on my own, so I used the Internet
+            fun calcPossibleResults(nextIndex: Int = 0, current: Long = 0L): List<Long> {
+                if (nextIndex == numbers.size) {
+                    //if (current == requiredResult) totalSum += requiredResult
+                    return listOf(current)
+                }
 
-            val nextNumber = numbers[nextIndex]
-            return calcPossibleResults(
-                nextIndex + 1, current + nextNumber
-            ) + calcPossibleResults(
-                nextIndex + 1, current * nextNumber
-            ) + calcPossibleResults(
-                nextIndex + 1, "$current$nextNumber".toLong()
-            )
-        }
+                val nextNumber = numbers[nextIndex]
+                return calcPossibleResults(
+                    nextIndex + 1, current + nextNumber
+                ) + calcPossibleResults(
+                    nextIndex + 1, current * nextNumber
+                ) + calcPossibleResults(
+                    nextIndex + 1, "$current$nextNumber".toLong()
+                )
+            }
 
-        if (requiredResult in calcPossibleResults()) {
-            bigSum = bigSum.add(BigInteger.valueOf(requiredResult))
+            if (requiredResult in calcPossibleResults()) bigSum += requiredResult
         }
     }
+
     println(bigSum)
 }
